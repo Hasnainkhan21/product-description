2a6397a2037286cb39fac9a39e13522d00e4a828
Muhammad Hammad Ali <mh6139251@gmail.com>
2025-09-01 02:58:19 -0700

    Added AI description generator using OpenAI API

--- Backend/Router/productRoutes.js
+++ Backend/Router/productRoutes.js
@@ -2,13 +2,9 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const path = require('path');
-<<<<<<< HEAD
 
 const { createProduct, getProducts,generateDescription } = require('../Controllers/productController');
 router.post("/generate-description", generateDescription);
-=======
-const { createProduct, getProducts } = require('../Controllers/productController');
->>>>>>> 2895f281d6f2b76d77f819e07d46309d4717fae5
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
